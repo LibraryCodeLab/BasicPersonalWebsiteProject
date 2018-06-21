a35defe4c6d54b526eb095fd77d42aa80d1dd1c6
cneymoss <cneymoss@cmlibrary.org>
2018-06-21 14:12:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function turnGreen () {document.getElementById("header").style.backgroundColor="green";}
+function turnOrange () {document.getElementById("header").style.backgroundColor="orange";}
+function turnYellow () {document.getElementById("header").style.backgroundColor="yellow";}
+
