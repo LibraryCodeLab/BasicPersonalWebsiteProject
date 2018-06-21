9a2981378390f6b42495f8908046164a553eb78a
cneymoss <cneymoss@cmlibrary.org>
2018-06-21 14:14:07 -0400

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-function turnGreen () {document.getElementById("header").style.backgroundColor="green";}
-function turnOrange () {document.getElementById("header").style.backgroundColor="orange";}
-function turnYellow () {document.getElementById("header").style.backgroundColor="yellow";}
-
